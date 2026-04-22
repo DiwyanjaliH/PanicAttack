af73f459d2cc9ccccba4285d4542cb59e721d1f2
Hiroshima Diwyanjali <168516461+DiwyanjaliH@users.noreply.github.com>
2026-04-22 06:23:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+const submitBtn = document.getElementById("submitBtn");
+const notification = document.getElementById("notification");
+const timerDisplay = document.getElementById("timer");
+
+const dropboxLink = "https://www.dropbox.com/request/00ncbhy37m7yeje128nl";
+
+// ⏳ 30 minutes = 1800 seconds
+let timeLeft = 30 * 60;
+
+let countdown = setInterval(() => {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+
+  // format 00:00
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  timerDisplay.innerText = `${minutes}:${seconds}`;
+
+  timeLeft--;
+
+  // ⚠️ time up
+  if (timeLeft < 0) {
+    clearInterval(countdown);
+    timerDisplay.innerText = "⛔ Time's Up!";
+    submitBtn.disabled = true;
+
+    notification.classList.remove("hidden");
+    notification.innerText = "❌ Submission time is over!";
+  }
+
+  // 🔴 last 1 minute warning
+  if (timeLeft <= 60) {
+    timerDisplay.style.color = "#ff4757";
+  }
+
+}, 1000);
+
+// submit button
+submitBtn.addEventListener("click", () => {
+  notification.classList.remove("hidden");
+  notification.innerText = "✅ Redirecting to submit...";
+
+  setTimeout(() => {
+    window.location.href = dropboxLink;
+  }, 1000);
+});
